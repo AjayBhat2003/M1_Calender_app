b4955e2dda35b1d1b542cb90e8105cf34202f51e
AjayBhat2003 <AjayBhat2003@gmail.com>
2022-03-03 18:59:14 +0530

    Added description for Milestone-1,#1

--- 3_Implementation/test/test_calender.c
+++ 3_Implementation/test/test_calender.c
@@ -12,14 +12,19 @@ void teardown()
 }
 void test_dayNumber()
 { 
-    s
+    int year=2022,day=28,month=02;
+    dayNumber(28,02,2022);
 }
+
+
+
+    
 
 int test_main()
 {
     UNITY_BEGIN();
     RUN_TEST(test_dayNumber);
-    RUN_TEST()
+
 
     return UNITY_END();
 
