bc11c7d0ebbcc33163ed6683e5432b1ad6e73282
AjayBhat2003 <AjayBhat2003@gmail.com>
2022-03-04 14:36:59 +0530

    Added description for  Milestone-1,#1

--- 3_Implementation/Multifile/test_calender.c
+++ 3_Implementation/Multifile/test_calender.c
@@ -1,30 +1,31 @@
 #include "unity.h"
 #include "calender.h"
 
-void setup()
+void setUp()
 {
 
 }
 
-void teardown()
+void tearDown()
 {
 
 }
-void test_dayNumber(void)
-{ int year=2022,day=28,month=02;
-    dayNumber(28,02,2022);
+void test_dayNumber()
+{ 
+    
+  TEST_ASSERT_EQUAL_DOUBLE_ARRAY( 28,02,2022);
 }
 
-void test_printCalender(void)
-{
 
-}
+
+    
 
 int test_main()
 {
     UNITY_BEGIN();
     RUN_TEST(test_dayNumber);
-    RUN_TEST(test_printCalender);
+
+
     return UNITY_END();
 
 }
